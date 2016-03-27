8707413cf68efd728fce16618963d7c5f6270161
CaveeJohnson <caveejohnson@hotmail.com>
2016-03-27 13:34:47 +0100

    hexbox shit

---
+++ lua/autorun/hexbox.lua
@@ -0,0 +1,251 @@
+hexbox = {}
+
+if CLIENT then
+	surface.CreateFont("hexbox_console", {
+		font = "Courier",
+		size = 14,
+		antialiasing = false,
+		weight = 400,
+	})
+
+else
+	require"sourcenetinfo"
+	require"cvar3"
+	require"usercmd"
+	require"dns"
+
+	dns.__lookup = dns.__lookup or dns.Lookup
+	function dns.Lookup(name)
+		return dns.__lookup(name, "8.8.8.8")
+	end
+
+	dns.__ping = dns.__ping or dns.Ping
+	function dns.Ping(num)
+		return dns.__ping("8.8.8.8", num)
+	end
+
+	function dns.IsHostOnline(str)
+		local Online, err, code = dns.Ping(300)
+
+		if not Online then
+			return false, "Pinging google dns (8.8.8.8) failed! ("..err..(code and ", "..code or "")..")"
+		end
+
+
+		local Addr, err, code = dns.Lookup(str)
+		if not Addr then
+			return false, "Lookup "..str.." failed! ("..err..(code and ", "..code or "")..")"
+		end
+
+		return true, Addr
+	end
+end
+
+hexbox.extraWeps = {
+	"none",
+	"torch"
+}
+hexbox.nuke = {
+	["weapon_357"] = true,
+	["weapon_ar2"] = true,
+	["weapon_bugbait"] = true,
+	["weapon_crossbow"] = true,
+	["weapon_frag"] = true,
+	["weapon_pistol"] = true,
+	["weapon_rpg"] = true,
+	["weapon_shotgun"] = true,
+	["weapon_slam"] = true,
+	["weapon_smg1"] = true,
+	["weapon_stunstick"] = true,
+	["manhack_welder"] = true,
+	["weapon_medkit"] = true,
+}
+hexbox.nuke_ent = {
+	["item_ammo_ar2"] = true,
+	["item_ammo_pistol"] = true,
+	["item_box_buckshot"] = true,
+	["item_ammo_357"] = true,
+	["item_ammo_smg1"] = true,
+	["item_ammo_ar2_altfire"] = true,
+	["item_ammo_crossbow"] = true,
+	["item_ammo_smg1_grenade"] = true,
+	["item_rpg_round"] = true,
+
+	["item_battery"] = true,
+	["item_healthvial"] = true,
+	["item_healthkit"] = true,
+	["item_suitcharger"] = true,
+	["item_healthcharger"] = true,
+	["item_suit"] = true,
+
+	["prop_thumper"] = true,
+	["combine_mine"] = true,
+	["grenade_helicopter"] = true,
+	["npc_grenade_frag"] = true,
+
+	["sent_ball"] = true,
+}
+
+function hexbox.loadout(ply)
+	for k, v in ipairs(hexbox.extraWeps) do ply:Give(v) end
+end
+if SERVER then hook.Add("PlayerLoadout", "hexbox_loadout", hexbox.loadout) end
+
+function hexbox.nukeWeapons()
+	local weps = list.GetForEdit"Weapon"
+	for k, v in pairs(weps) do
+		if hexbox.nuke[k] then weps[k] = nil end
+	end
+end
+hook.Add("InitPostEntity", "hexbox_nukeweapons", hexbox.nukeWeapons)
+
+function hexbox.nukeEnts()
+	local ents = list.GetForEdit"SpawnableEntities"
+	for k, v in pairs(ents) do
+		if hexbox.nuke_ent[k] then ents[k] = nil end
+	end
+end
+hook.Add("InitPostEntity", "hexbox_nukeents", hexbox.nukeEnts)
+
+function hexbox.centerHack()
+	for k, v in ipairs(ents.FindByClass("prop_*")) do
+		if v.centergotten then continue end
+
+		local p = v:GetPhysicsObject()
+		if not (p and p:IsValid()) then continue end
+
+		local c = p:GetMassCenter()
+		if not c then continue end
+
+		v.centergotten = true
+		v:SetNW2Vector("obj_center", c)
+	end
+end
+if SERVER then hook.Add("Think", "hexbox_centerhack", hexbox.centerHack) end
+
+color_red = Color(255, 0, 0, 255)
+color_green = Color(0, 255, 0, 255)
+color_blue = Color(0, 0, 255, 255)
+
+if CLIENT then -- HUD
+	hexbox.hide = {
+		CHudAmmo = true,
+		CHudBattery = true,
+		CHudHealth = true,
+		CHudPoisonDamageIndicator = true,
+	}
+
+	local centers = CreateClientConVar("hexbox_centers", "0", true)
+
+	function hexbox.hud()
+		local ply = LocalPlayer()
+		local x, y = 5, ScrH() - 5
+		local str, tW, tH
+
+		local name = ply:Nick():lower():Trim():gsub(" ", "_") .. "_pc"
+
+		local maxvelo = 3000
+		local segments = #name + 13
+		local prepend = math.floor(math.log10(maxvelo))
+
+		local time = ply:GetPlayTimeTable()
+		time = (time.h < 10 and "0" .. time.h or time.h) .. ":" .. (time.m < 10 and "0" .. time.m or time.m) .. " h"
+
+		local velo = math.floor(ply:GetAbsVelocity():Length())
+		str = math.min(velo, maxvelo)
+
+		local actualprepend = prepend - math.floor(math.log10(velo))
+
+		local used = math.floor(str / maxvelo * segments)
+		local left = segments - used
+
+		str = ("#"):rep(used)
+		str = time .. "    [" .. str .. ("-"):rep(left) .. "] " .. (velo ~= 0 and ("0"):rep(actualprepend) .. velo or ("0"):rep(prepend) .. "0") .. " u/s"
+
+		surface.SetFont("hexbox_console")
+		tW, tH = surface.GetTextSize(str)
+
+		y = y - tH - 2
+
+		surface.SetDrawColor(color_black)
+		surface.DrawRect(x, y - 1, tW + 6, tH + 2)
+
+		surface.SetTextColor(color_white)--Color(50, 255, 100, 255))
+		surface.SetTextPos(x + 3, y - 1)
+		surface.DrawText(str)
+
+		y = y - tH - 2
+
+		str = "[root@" .. name .. " hexbox]$ gmod_track_stats.sh"
+
+		surface.SetFont("hexbox_console")
+		tW, tH = surface.GetTextSize(str)
+
+		surface.SetDrawColor(color_black)
+		surface.DrawRect(x, y - 1, tW + 6, tH + 2)
+
+		surface.SetTextColor(color_white)
+		surface.SetTextPos(x + 3, y)
+		surface.DrawText(str)
+
+		if centers:GetBool() then
+			for k, v in ipairs(ents.FindByClass("prop_*")) do
+				local p = v.obj_center
+				if not p then p = v:GetNW2Vector("obj_center") v.obj_center = p end
+
+				if p then
+					local c = v:LocalToWorld(p)
+
+					local f = (c + v:GetAngles():Forward() * 10):ToScreen()
+					local r = (c + v:GetAngles():Right() * 10):ToScreen()
+					local u = (c + v:GetAngles():Up() * 10):ToScreen()
+
+					c = c:ToScreen()
+
+					if c then
+						surface.SetDrawColor(color_red)
+						surface.DrawLine(c.x, c.y, f.x, f.y)
+
+						surface.SetDrawColor(color_blue)
+						surface.DrawLine(c.x, c.y, r.x, r.y)
+
+						surface.SetDrawColor(color_green)
+						surface.DrawLine(c.x, c.y, u.x, u.y)
+					end
+				end
+			end
+		end
+	end
+	hook.Add("HUDPaint", "hexbox_hud", hexbox.hud)
+
+	function hexbox.hideHUDElements(element)
+		if hexbox.hide[element] then return false end
+	end
+	hook.Add("HUDShouldDraw", "hexbox_hidehudelements", hexbox.hideHUDElements)
+
+	local gm = gmod.GetGamemode() or GM or GAMEMODE or {}
+	function gm:HUDDrawTargetID()
+
+	end
+
+	local autoJump = CreateClientConVar("hexbox_autojump", "0", true)
+
+	function hexbox.autojump(cmd)
+		if not autoJump:GetBool() then return end
+
+		local ply = LocalPlayer()
+		if ply:GetMoveType() == MOVETYPE_NOCLIP or ply:WaterLevel() > 1 then return end
+
+		if not ply:OnGround() then
+			cmd:SetButtons(bit.band(cmd:GetButtons(), bit.bnot(IN_JUMP)))
+		end
+	end
+	hook.Add("CreateMove", "hexbox_autojump", hexbox.autojump)
+end
+
+
+-- HUD DONE
+-- Fix physclip
+-- Tool searching
+-- ENT Blacklist DONE
+-- ACF config DONE
---
+++ lua/autorun/propshit.lua
@@ -0,0 +1,210 @@
+local modelBlacklist = {
+	["models/props_vehicles/tanker001a.mdl"] = true,
+	["models/props_vehicles/apc001.mdl"] = true,
+	["models/props_combine/combinetower001.mdl"] = true,
+	["models/cranes/crane_frame.mdl"] = true,
+	["models/items/item_item_crate.mdl"] = true,
+	["models/props/cs_militia/silo_01.mdl"] = true,
+	["models/props/cs_office/microwave.mdl"] = true,
+	["models/props/de_train/biohazardtank.mdl"] = true,
+	["models/props_buildings/building_002a.mdl"] = true,
+	["models/props_buildings/collapsedbuilding01a.mdl"] = true,
+	["models/props_buildings/project_building01.mdl"] = true,
+	["models/props_buildings/row_church_fullscale.mdl"] = true,
+	["models/props_c17/consolebox01a.mdl"] = true,
+	["models/props_c17/oildrum001_explosive.mdl"] = true,
+	["models/props_c17/paper01.mdl"] = true,
+	["models/props_c17/trappropeller_engine.mdl"] = true,
+	["models/props_canal/canal_bridge01.mdl"] = true,
+	["models/props_canal/canal_bridge02.mdl"] = true,
+	["models/props_canal/canal_bridge03a.mdl"] = true,
+	["models/props_canal/canal_bridge03b.mdl"] = true,
+	["models/props_combine/combine_citadel001.mdl"] = true,
+	["models/props_combine/combine_mine01.mdl"] = true,
+	["models/props_combine/combinetrain01.mdl"] = true,
+	["models/props_combine/combinetrain02a.mdl"] = true,
+	["models/props_combine/combinetrain02b.mdl"] = true,
+	["models/props_combine/prison01.mdl"] = true,
+	["models/props_combine/prison01c.mdl"] = true,
+	["models/props_industrial/bridge.mdl"] = true,
+	["models/props_junk/garbage_takeoutcarton001a.mdl"] = true,
+	["models/props_junk/gascan001a.mdl"] = true,
+	["models/props_junk/glassjug01.mdl"] = true,
+	["models/props_junk/trashdumpster02.mdl"] = true,
+	["models/props_phx/amraam.mdl"] = true,
+	["models/props_phx/ball.mdl"] = true,
+	["models/props_phx/cannonball.mdl"] = true,
+	["models/props_phx/huge/evildisc_corp.mdl"] = true,
+	["models/props_phx/misc/flakshell_big.mdl"] = true,
+	["models/props_phx/misc/potato_launcher_explosive.mdl"] = true,
+	["models/props_phx/mk-82.mdl"] = true,
+	["models/props_phx/oildrum001_explosive.mdl"] = true,
+	["models/props_phx/torpedo.mdl"] = true,
+	["models/props_phx/ww2bomb.mdl"] = true,
+	["models/props_wasteland/cargo_container01.mdl"] = true,
+	["models/props_wasteland/cargo_container01.mdl"] = true,
+	["models/props_wasteland/cargo_container01b.mdl"] = true,
+	["models/props_wasteland/cargo_container01c.mdl"] = true,
+	["models/props_wasteland/depot.mdl"] = true,
+	["models/xqm/coastertrack/special_full_corkscrew_left_4.mdl"] = true,
+	["models/props_junk/propane_tank001a.mdl"] = true,
+	["models/props_c17/fountain_01.mdl"] = true,
+	["models/props_trainstation/train003.mdl"] = true,
+	["models/props_foliage/tree_poplar_01.mdl"] = true,
+	["models/mechanics/solid_steel/i_beam2_32.mdl"] = true,
+	["models/props_c17/furnituredrawer001a_chunk06.mdl"] = true,
+	["models/mechanics/solid_steel/i_beam2_32.mdl"] = true,
+	["models/props_phx/mechanics/slider2.mdl"] = true,
+	["models/props_phx/gears/rack70.mdl"] = true,
+	["models/mechanics/gears2/pinion_80t1.mdl"] = true,
+	["models/nova/airboat_seat.mdl"] = true,
+	["models/mechanics/robotics/a4.mdl"] = true,
+	["models/mechanics/roboticslarge/claw_hub_8.mdl"] = true,
+	["models/perftest/loader_static.mdl"] = true,
+	["models/mechanics/robotics/e4.mdl"] = true,
+	["models/mechanics/roboticslarge/e4.mdl"] = true,
+	["models/perftest/rocksground01b.mdl"] = true,
+	["models/mechanics/roboticslarge/g4.mdl"] = true,
+	["models/mechanics/roboticslarge/e4.mdl"] = true,
+	["models/mechanics/roboticslarge/j4.mdl"] = true,
+	["models/props_animated_breakable/smokestack.mdl"] = true,
+	["models/props_animated_breakable/smokestack_gib_01.mdl"] = true,
+	["models/xqm/rails/slope_down_90.mdl"] = true,
+	["models/props_animated_breakable/smokestack_gib_02.mdl"] = true,
+	["models/props_animated_breakable/smokestack_gib_03.mdl"] = true,
+	["models/props_animated_breakable/smokestack_gib_04.mdl"] = true,
+	["models/props_animated_breakable/smokestack_gib_05.mdl"] = true,
+	["models/props_animated_breakable/smokestack_gib_06.mdl"] = true,
+	["models/props_animated_breakable/smokestack_gib_07.mdl"] = true,
+	["models/props_animated_breakable/smokestack_gib_08.mdl"] = true,
+	["models/xqm/coastertrack/special_full_loop_3.mdl"] = true,
+	["models/props_animated_breakable/smokestack_gib_09.mdl"] = true,
+	["models/props_animated_breakable/smokestack_gib_10.mdl"] = true,
+	["models/xqm/coastertrack/special_full_corkscrew_right_4.mdl"] = true,
+	["models/props_buildings/collapsedbuilding01awall.mdl"] = true,
+	["models/props_buildings/collapsedbuilding02a.mdl"] = true,
+	["models/props_buildings/collapsedbuilding02b.mdl"] = true,
+	["models/xqm/coastertrack/special_half_corkscrew_right_4.mdl"] = true,
+	["models/props_buildings/collapsedbuilding02c.mdl"] = true,
+	["models/props_buildings/project_destroyedbuildings01.mdl"] = true,
+	["models/props_buildings/project_building03_skybox.mdl"] = true,
+	["models/props_buildings/project_building03.mdl"] = true,
+	["models/props_buildings/project_building02_skybox.mdl"] = true,
+	["models/props_buildings/project_building02.mdl"] = true,
+	["models/props_buildings/project_building01_skybox.mdl"] = true,
+	["models/props_buildings/factory_skybox001a.mdl"] = true,
+	["models/xqm/coastertrack/special_full_corkscrew_right_3.mdl"] = true,
+	["models/props_buildings/row_res_1_fullscale.mdl"] = true,
+	["models/props_buildings/watertower_002a.mdl"] = true,
+	["models/props_buildings/watertower_001c.mdl"] = true,
+	["models/props_buildings/watertower_001a.mdl"] = true,
+	["models/props_buildings/short_building001a.mdl"] = true,
+	["models/props_buildings/row_res_2_fullscale.mdl"] = true,
+	["models/props_buildings/row_res_2_ascend_fullscale.mdl"] = true,
+	["models/xqm/coastertrack/special_full_corkscrew_left_2.mdl"] = true,
+	["models/props_canal/generator01.mdl"] = true,
+	["models/props_canal/generator02.mdl"] = true,
+	["models/props_canal/locks_large.mdl"] = true,
+	["models/props_canal/locks_large_b.mdl"] = true,
+	["models/props_canal/locks_small.mdl"] = true,
+	["models/props_canal/locks_small_b.mdl"] = true,
+	["models/xqm/coastertrack/special_half_corkscrew_right_4.mdl"] = true,
+	["models/props_canal/canal_bars001.mdl"] = true,
+	["models/props_trainstation\train003.mdl"] = true,
+	["models/props_canal/canal_bridge04.mdl"] = true,
+	["models/props_canal/pipe_bracket001.mdl"] = true,
+	["models/props_canal/canal_bridge_railing_lamps.mdl"] = true,
+	["models/props_canal/canal_bridge_railing02.mdl"] = true,
+	["models/props_canal/canal_bridge_railing01.mdl"] = true,
+	["models/xqm/coastertrack/special_half_corkscrew_right_3.mdl"] = true,
+	["models/props_canal/winch01.mdl"] = true,
+	["models/props_canal/rock_riverbed01c.mdl"] = true,
+	["models/props_canal/rock_riverbed01d.mdl"] = true,
+	["models/props_canal/rock_riverbed02a.mdl"] = true,
+	["models/props_canal/rock_riverbed02b.mdl"] = true,
+	["models/props_canal/winch02c.mdl"] = true,
+	["models/props_canal/winch02d.mdl"] = true,
+	["models/props_canal/rock_riverbed01b.mdl"] = true,
+	["models/props_canal/refinery_04.mdl"] = true,
+	["models/props_canal/refinery_05.mdl"] = true,
+	["models/xqm/rails/twist_90_left.mdl"] = true,
+	["models/props_canal/canal_bars001.mdl"] = true,
+	["models/props_canal/bridge_pillar02.mdl"] = true,
+	["models/xqm/rails/loop_right.mdl"] = true,
+	["models/props_citizen_tech/windmill_blade002a.mdl"] = true,
+	["models/props_citizen_tech/till001a_base01.mdl"] = true,
+	["models/props_citizen_tech/steamengine001a.mdl"] = true,
+	["models/props_citizen_tech/guillotine001a_base01.mdl"] = true,
+	["models/props_citizen_tech/firetrap_gashose01c.mdl"] = true,
+	["models/props_citizen_tech/firetrap_gashose01b.mdl"] = true,
+	["models/props_citizen_tech/firetrap_button01a.mdl"] = true,
+	["models/props_citizen_tech/windmill_blade004a.mdl"] = true,
+	["models/props_phx/misc/potato_launcher_chamber.mdl"] = true,
+	["models/props_combine/combine_train02a.mdl"] = true,
+}
+
+local noSounds = {
+	"vo/engineer_no01.mp3",
+	"vo/engineer_no02.mp3",
+	"vo/engineer_no03.mp3",
+}
+
+local log = CreateConVar("propshit_log", "1", FCVAR_ARCHIVE, "Enables the logging of prop spawns by propshit.")
+
+local function PlayerSpawnProp(ply, model)
+	local escapedModel = model:lower():gsub("\\","/"):gsub("//", "/"):Trim()
+	if modelBlacklist[escapedModel] then
+		if SERVER then ply:EmitSound(noSounds[math.random(1, #noSounds)], 140) end
+	return false end
+
+	if SERVER and log:GetBool() then print("PROP EVENT: ", ply, " -> ", escapedModel) end
+
+	return true
+end
+hook.Add("PlayerSpawnProp", "propshit", PlayerSpawnProp)
+
+local function CanDrive()
+	return false
+end
+hook.Add("CanDrive", "propshit", CanDrive)
+
+local function PlayerSpawnNPC()
+	return false
+end
+hook.Add("PlayerSpawnNPC", "propshit", PlayerSpawnNPC)
+
+local function PlayerSpawnRagdoll()
+	return false
+end
+hook.Add("PlayerSpawnRagdoll", "propshit", PlayerSpawnRagdoll)
+
+local spam = CurTime()
+local function CanTool(ply, trace, mode)
+	if mode == "duplicator" then
+		if SERVER then ply:ChatPrint("The duplicator is not allowed here. Build your own stuff or use advdupe2!") end
+	return false end
+
+	if mode == "paint" then
+		if SERVER and spam < CurTime() - 1 then ply:ChatPrint("Paint is banned here, there is no legitimate use for it.") spam = CurTime() end
+	return false end
+
+	if mode == "physprop" and trace.Entity:IsValid() and trace.Entity:GetClass() == "prop_vehicle_jeep" then
+		return false
+	end
+
+	if trace.Entity.m_tblToolsAllowed then
+		local vFound = false
+		for k, v in pairs(trace.Entity.m_tblToolsAllowed) do
+			if mode == v then vFound = true end
+		end
+
+		if not vFound then return false end
+	end
+
+	if trace.Entity.CanTool then
+		return trace.Entity:CanTool(ply, trace, mode)
+	end
+
+	return true
+end
+hook.Add("CanTool", "propshit", CanTool)

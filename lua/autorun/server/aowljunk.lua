ec2949528f86fef25bc759d897a42e34f2df9b1d
Andrew Austin <ghosty.hexahedronic@gmail.com>
2016-03-28 22:19:56 +0100

    cuck

--- lua/autorun/server/aowljunk.lua
+++ lua/autorun/server/aowljunk.lua
@@ -1,5 +1,5 @@
 local function add()
-	require"cvar3"
+	pcall(require, "cvar3")
 	if cvar3 or debug.getregistry().ConVar.SetValue then -- hack
 		local function SetConVar(cvar,val)
 			local cv = GetConVar(cvar)
@@ -24,23 +24,26 @@ local function add()
 			StripFlags(cvar, FCVAR_CHEAT)
 		end, "developers")
 	end
-	aowl.AddCommand("stoppackets",function(ply,_,who_r,time)
-		time = tonumber(time or "") or 5
-		time = math.ceil(time)
-		local who = easylua.FindEntity(who_r)
-		if not who then return false, aowl.TargetNotFound(who_r) end
-		timer.Create("ddos_" .. ply:UniqueID(), 0.001, time * 100, function()
-			if IsValid(who) then
-				who:GetNetChannel():Reset()
-				who:GetNetChannel():Transmit(false)
-			end
-		end)
-		timer.Simple(time, function()
-			if IsValid(who) then
-				who:GetNetChannel():Transmit(true)
-			end		
-		end)
-	end, "developers")
+	pcall(require, "sourcenetinfo")
+	if sourcenetinfo then
+		aowl.AddCommand("stoppackets",function(ply,_,who_r,time)
+			time = tonumber(time or "") or 5
+			time = math.ceil(time)
+			local who = easylua.FindEntity(who_r)
+			if not who then return false, aowl.TargetNotFound(who_r) end
+			timer.Create("ddos_" .. ply:UniqueID(), 0.001, time * 100, function()
+				if IsValid(who) then
+					who:GetNetChannel():Reset()
+					who:GetNetChannel():Transmit(false)
+				end
+			end)
+			timer.Simple(time, function()
+				if IsValid(who) then
+					who:GetNetChannel():Transmit(true)
+				end		
+			end)
+		end, "developers")
+	end
 end
 
 if aowl then
